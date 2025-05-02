19940d1979b203320118aebc1e36b06d40c2c4e8
Jianhai Su <suj@email.sc.edu>
2025-05-01 23:38:13 -0400

    update run_dqn.py

--- run_dqn.py
+++ run_dqn.py
@@ -11,16 +11,21 @@ from tqdm import tqdm
 from dqn import train_dqn
 from utility import plot_mean_sem
 import pickle
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--map_size", type=str, required=True, help="e.g., 2x2, 3x3, 4x4")
+args = parser.parse_args()
 
 # run experiments
-map_name="3x3"
+map_name=args.map_size #"3x3"
 max_steps=100000 #20000
 eval_interval=1000
 #seed=42
-buffer_paths={1: "Map3x3_seed1_deepseek-llm-7b-chat-GPTQ_HWS5_PO_NotChatGPT_NoFewshot_NoThought_100Episodes_520Trans_0.17AveR.pkl"}
+buffer_paths=None
+#{1: "Map3x3_seed1_deepseek-llm-7b-chat-GPTQ_HWS5_PO_NotChatGPT_NoFewshot_NoThought_100Episodes_520Trans_0.17AveR.pkl"}
 
-seeds=[1] #, 2, 3, 4, 5]
+seeds=[1, 2, 3, 4, 5]
 all_eval_returns = {}
 for seed in seeds:
     if buffer_paths:
@@ -33,7 +38,7 @@ for seed in seeds:
         buffer_path=buffer_path)
     all_eval_returns[seed] = eval_returns
 
-'''
+
 label = f"Map{map_name}"
 if buffer_paths:
     label += "_withLLMTrajs"
@@ -44,4 +49,4 @@ plot_mean_sem(all_eval_returns, label=label, color=color, save_path=save_path)
 # Save to file
 with open(save_path[:-4]+".pkl", "wb") as f:
     pickle.dump(all_eval_returns, f)
-'''
+
